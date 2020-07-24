d6984d812c77fabc8143c733aa325f1a28d96405
Zach Choate <zach.a.choate@gmail.com>
2020-07-24 10:14:51 -0400

    Additional updates to docs

--- Install-LocalPrinters.ps1
+++ Install-LocalPrinters.ps1
@@ -7,6 +7,9 @@
     When a printer needs to be added to a deployment, add the drivers to the container, update the CSV,
         and update the deployDate check on the Intune app deployment.
     CSV should have the following headers:
+        install:
+            - set to 0 to remove the printer (only IP or printerName are required)
+            - set to 1 to install the printer
         printerName - display name of the printer
         printerIP - IP address of the printer
         driverFilePath - path to driver (inf and other dependent files) - printernamefolder\printerdriver.inf
@@ -21,6 +24,12 @@
             - valid values: None, StapleTopLeft, StapleTopRight, StapleBottomLeft, StapleBottomRight, StapleDualLeft,
 StapleDualRight, StapleDualTop, StapleDualBottom
             - refer to https://docs.microsoft.com/en-us/windows/win32/printdocs/jobstaplealldocuments
+        custSharpUd3Staple:
+            - valid values: None, 1 Staple, 1 Staple/2 Staples, 1/2/Saddle Staple, 1 Staple/2 Staples/Stapless, 1/2/Saddle/Stapless
+            - tested with Sharp Universal Driver v3
+        custSharpUd3Punch:
+            - valid values: None, 2 Holes, 3 Holes, 4 Holes, 4 Holes Wide, 2/3 Holes, 2/4 Holes
+            - tested with Sharp Universal Driver v3
 .PARAMETER blobSAS
     This should be the URL that the contents of the printer deployment are to be downloaded from.
     For Azure: container URL + SAS token
@@ -31,11 +40,15 @@ StapleDualRight, StapleDualTop, StapleDualBottom
     This will set a registry key at HKLM:\SOFTWARE\PrinterDeploy with the value specified here.
     Use this as a check that the most current deployment is installed.
 .NOTES
-    Version:         2.0
-    Last updated:    07/20/2020
+
+    BREAKING CHANGES between 1.x and 2.x. CSVs should be updated to include at least the install header.
+
+    Version:         2.1
+    Last updated:    07/24/2020
     Creation Date:   02/12/2020
     Author:          Zachary Choate
-    URL:             https://raw.githubusercontent.com/zchoate/Install-LocalPrinters/main/Install-LocalPrinters.ps1
+    URL:             https://raw.githubusercontent.com/KSMC-TS/Install-LocalPrinters/main/Install-LocalPrinters.ps1
+
 #>
 
 param(
@@ -175,7 +188,7 @@ Function Set-SharpDriverModules {
                 "1 Staple/2 Staples" { $value = 2 }
                 "1/2/Saddle Staple" { $value = 4 }
                 "1 Staple/2 Staples/Stapless" { $value = 10 }
-                "1/2/Saddle/Staplesss" { $value = 12 }
+                "1/2/Saddle/Stapless" { $value = 12 }
             }
         }
     }
