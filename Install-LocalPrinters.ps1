3a1a6bbb3342da5567aa0a80d7d34262c182feb8
Zach Choate <zach.a.choate@gmail.com>
2020-07-24 09:30:43 -0400

    v2.0!
    Sharp specific enhancements. Removal of existing printers option.

--- Install-LocalPrinters.ps1
+++ Install-LocalPrinters.ps1
@@ -28,11 +28,11 @@ StapleDualRight, StapleDualTop, StapleDualBottom
         printerDeploy.csv with printers to be deployed/redeployed
         folders for each printer containing driver files
 .PARAMETER deployDate
-    This will set a registry key at HKLM:\SOFTWARE\WOW6432Node\printerDeploy with the value specified here.
+    This will set a registry key at HKLM:\SOFTWARE\PrinterDeploy with the value specified here.
     Use this as a check that the most current deployment is installed.
 .NOTES
-    Version:         1.5
-    Last updated:    03/04/2020
+    Version:         2.0
+    Last updated:    07/20/2020
     Creation Date:   02/12/2020
     Author:          Zachary Choate
     URL:             https://raw.githubusercontent.com/zchoate/Install-LocalPrinters/main/Install-LocalPrinters.ps1
@@ -48,7 +48,7 @@ function Install-LocalPrinter {
     Param($driverName,$driverFilePath,$printerIP,$printerName)
 
     #Install Printer Driver using PNP and Add-PrinterDriver
-    Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList "& c:\windows\sysnative\pnputil.exe /add-driver $driverFilePath /install /force" -RedirectStandardOutput "$env:TEMP\printerDeploy\pnpOutput.txt" -NoNewWindow -Wait 
+    Start-Process "powershell.exe" -ArgumentList "& c:\windows\system32\pnputil.exe /add-driver $driverFilePath /install /force" -RedirectStandardOutput "$env:TEMP\printerDeploy\pnpOutput.txt" -NoNewWindow -Wait 
     [string]$pnpOutput = (Get-Content "$env:TEMP\printerDeploy\pnpOutput.txt") -match "Published name:\s*(?<name>.*\.inf)"
     $driverInf = ($pnpOutput.Split(":") -match ".inf").Trim()
     $driverInf = "C:\Windows\INF\$driverInf"
@@ -110,13 +110,95 @@ function Set-PrintConfigurationSetting {
         Set-PrintConfiguration -PrinterName $printerName -PrintTicketXml $updatedConfig
 }
 
+Function Remove-LocalPrinters {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $printerIp,
+        [Parameter()]
+        [string]
+        $printerName
+    )
+
+    If($printerIp) {
+        $printerToRemove = Get-Printer | Where-Object {$_.PortName -like "*$printerIp*"}
+    } elseif ($printerName) {
+        $printerToRemove = Get-Printer | Where-Object {$_.Name -like "*$printerName*" -or $_.ShareName -like "*$printerName*"}
+    } else {
+        Write-Output "No printer IP or name was specified. Try passing either one as a parameter."
+    }
+
+    If($printerToRemove) {
+        $printerPortName = $printerToRemove.PortName
+        Remove-Printer -Name $printerToRemove.Name
+        Remove-PrinterPort -Name $printerPortName
+    } else {
+        Write-Output "Printer $printerIp $printerName doesn't appear to be installed."
+    }
+}
+
+Function Set-SharpDriverModules {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $moduleType,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $moduleConfiguration,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $printerName
+    )
+
+    switch ( $moduleType ) {
+        "SharpUd3Punch" {
+            $basePath = "HKLM:\SYSTEM\CurrentControlSet\Control\Print\Printers\$printerName\PrinterDriverData"
+            $key = "dev_punchmodule_def"
+            switch ( $moduleConfiguration) {
+                "None" { $value = 0 }
+                "2 Holes" { $value = 1 }
+                "3 Holes" { $value = 6 }
+                "4 Holes" { $value = 7 }
+                "4 Holes Wide" { $value = 4 }
+                "2/3 Holes" { $value = 2 }
+                "2/4 Holes" { $value = 3 }
+            }
+        }
+        "SharpUd3Staple" {
+            $basePath = "HKLM:\SYSTEM\CurrentControlSet\Control\Print\Printers\$printerName\PrinterDriverData"
+            $key = "dev_staplemodule_def"
+            switch ( $moduleConfiguration ) {
+                "None" { $value = 0 }
+                "1 Staple" { $value = 1 }
+                "1 Staple/2 Staples" { $value = 2 }
+                "1/2/Saddle Staple" { $value = 4 }
+                "1 Staple/2 Staples/Stapless" { $value = 10 }
+                "1/2/Saddle/Staplesss" { $value = 12 }
+            }
+        }
+    }
+
+    New-ItemProperty -Path $basePath -Name $key -Value $value -PropertyType DWord -Force
+
+}
+
 New-Item -ItemType Directory -Path "$env:TEMP\printerDeploy" -Force
 Invoke-BlobItems -URL $blobSAS -Path "$env:TEMP\printerDeploy" | Out-Null
 Start-Sleep -s 300
 
 $logFile = "$env:TEMP\printerDeploy\printerDeploy.log"
 
-$printers = Import-Csv -Path "$env:TEMP\printerDeploy\printerDeploy.csv" -ErrorVariable PrinterDeployError
+$uninstallPrinters = Import-Csv -Path "$env:TEMP\printerDeploy\printerDeploy.csv" | Where-Object {$_.install -eq 0} -ErrorVariable PrinterDeployError
+
+ForEach ($uninstall in $uninstallPrinters) {
+
+    Remove-LocalPrinters -printerIp $uninstall.printerIp -printerName $uninstall.printerName -ErrorVariable PrinterDeployError
+
+}
+
+$printers = Import-Csv -Path "$env:TEMP\printerDeploy\printerDeploy.csv" | Where-Object {$_.install -eq 1} -ErrorVariable PrinterDeployError
 
 ForEach($printer in $printers) {
 
@@ -222,8 +304,13 @@ ForEach($printer in $printers) {
     # Set the default color setting for the printer if specified.
     If($printer.ColorSetting) {
 
-        Set-PrintConfigurationSetting -printerName $printer.PrinterName -configSetting "psk:PageOutputColor" -configValue $printer.ColorSetting
-        $deployOutput = $printer.PrinterName + " has had the color default set."
+        try {
+            Set-PrintConfigurationSetting -printerName $printer.PrinterName -configSetting "psk:PageOutputColor" -configValue $printer.ColorSetting -ErrorVariable PrinterDeployError
+            $deployOutput = $printer.PrinterName + " has had the color default set."
+        } catch {
+            $deployOutput = $printer.PrinterName + " failed to have the color default set."
+            $deployError = $deployOutput
+        }
         Write-Output "$(Get-TimeStamp) - $deployOutput" | Out-File $logFile -Append
 
     }
@@ -231,8 +318,13 @@ ForEach($printer in $printers) {
     # Set the default duplex setting for the printer if specified.
     If($printer.DuplexSetting) {
 
-        Set-PrintConfigurationSetting -printerName $printer.PrinterName -configSetting "psk:JobDuplexAllDocumentsContiguously" -configValue $printer.DuplexSetting
-        $deployOutput = $printer.PrinterName + " has had the duplex default set."
+        try {
+            Set-PrintConfigurationSetting -printerName $printer.PrinterName -configSetting "psk:JobDuplexAllDocumentsContiguously" -configValue $printer.DuplexSetting -ErrorVariable PrinterDeployError
+            $deployOutput = $printer.PrinterName + " has had the duplex default set."
+        } catch {
+            $deployOutput = $printer.PrinterName + " failed to have the duplex default set."
+            $deployError = $deployOutput
+        }
         Write-Output "$(Get-TimeStamp) - $deployOutput" | Out-File $logFile -Append
 
     }
@@ -240,12 +332,43 @@ ForEach($printer in $printers) {
     # Set the default staple setting for the printer if specified.
     If($printer.StapleSetting) {
 
-        Set-PrintConfigurationSetting -printerName $printer.PrinterName -configSetting "psk:JobStapleAllDocuments" -configValue $printer.StapleSetting
-        $deployOutput = $printer.PrinterName + " has had the staple default set."
+        try {
+            Set-PrintConfigurationSetting -printerName $printer.PrinterName -configSetting "psk:JobStapleAllDocuments" -configValue $printer.StapleSetting -ErrorVariable PrinterDeployError
+            $deployOutput = $printer.PrinterName + " has had the staple default set."
+        } catch {
+            $deployOutput = $printer.PrinterName + " failed to have the staple default set."
+            $deployError = $deployOutput
+        }
         Write-Output "$(Get-TimeStamp) - $deployOutput" | Out-File $logFile -Append
 
     }
 
+    # Enable Sharp staple module
+    If($printer.custSharpUd3Staple) {
+
+        try {
+            Set-SharpDriverModules -moduleType "SharpUd3Staple" -moduleConfiguration $printer.custSharpUd3Staple -printerName $printer.printerName -ErrorVariable PrinterDeployError
+            $deployOutput = $printer.PrinterName + " has had the staple module enabled."
+        } catch {
+            $deployOutput = $printer.PrinterName + " failed to have the staple module enabled."
+            $deployError = $deployOutput
+        }
+        Write-Output "$(Get-TimeStamp) - $deployOutput" | Out-File $logFile -Append
+
+    }
+
+    # Enable Sharp hole punch module
+    If($printer.custSharpUd3Punch) {
+        try {
+            Set-SharpDriverModules -moduleType "SharpUd3Punch" -moduleConfiguration $printer.custSharpUd3Punch -printerName $printer.printerName -ErrorVariable PrinterDeployError
+            $deployOutput = $printer.PrinterName + " has had the hole punch module enabled."
+        } catch {
+            $deployOutput = $printer.PrinterName + " failed to have the hole punch module enabled."
+            $deployError = $deployOutput
+        }
+        Write-Output "$(Get-TimeStamp) - $deployOutput" | Out-File $logFile -Append
+
+    }
 }
 
 If($PrinterDeployError) {
@@ -255,6 +378,5 @@ If($PrinterDeployError) {
 }
 
 Remove-Item -Recurse -Path "$env:TEMP\printerDeploy" -Force
-Set-Location -Path HKLM:
-New-Item -Path .\SOFTWARE -Name "printerDeploy" -Value $deployDate -Force
-Exit 0
+New-Item -Path "HKLM:\SOFTWARE" -Name "PrinterDeploy" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\PrinterDeploy" -Name "version_applied" -Value $deployDate -Force
